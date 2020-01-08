042d5a4dc0dff02c57cd701ffc83044470f4bfd7
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2020-01-08 10:57:03 +0100

    Adding initial migration.

---
+++ Microting.eFormBasicCaseManagementBase/Microting.eFormBasicCaseManagementBase/Migrations/20200108093744_InitialMigration.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Microting.eFormBasicCaseManagementBase.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {            
+            //Setup for SQL Server Provider
+           
+            string autoIDGenStrategy = "SqlServer:ValueGenerationStrategy";
+            object autoIDGenStrategyValue= SqlServerValueGenerationStrategy.IdentityColumn;
+
+            // Setup for MySQL Provider
+            if (migrationBuilder.ActiveProvider=="Pomelo.EntityFrameworkCore.MySql")
+            {
+                DbConfig.IsMySQL = true;
+                autoIDGenStrategy = "MySql:ValueGenerationStrategy";
+                autoIDGenStrategyValue = MySqlValueGenerationStrategy.IdentityColumn;
+            }
+
+            migrationBuilder.CreateTable(
+                name: "CalendarUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    Version = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    SiteId = table.Column<int>(nullable: false),
+                    IsVisibleInCalendar = table.Column<bool>(nullable: false),
+                    NameInCalendar = table.Column<string>(nullable: true),
+                    Color = table.Column<string>(nullable: true),
+                    RelatedEntityId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalendarUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CalendarUserVersions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    Version = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    SiteId = table.Column<int>(nullable: false),
+                    IsVisibleInCalendar = table.Column<bool>(nullable: false),
+                    NameInCalendar = table.Column<string>(nullable: true),
+                    Color = table.Column<string>(nullable: true),
+                    CalendarUserId = table.Column<int>(nullable: false),
+                    RelatedEntityId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CalendarUserVersions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PluginConfigurationValues",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    Version = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PluginConfigurationValues", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PluginConfigurationValueVersions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    Version = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PluginConfigurationValueVersions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PluginPermissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    Version = table.Column<int>(nullable: false),
+                    PermissionName = table.Column<string>(nullable: true),
+                    ClaimName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PluginPermissions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PluginGroupPermissions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    Version = table.Column<int>(nullable: false),
+                    GroupId = table.Column<int>(nullable: false),
+                    PermissionId = table.Column<int>(nullable: false),
+                    IsEnabled = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PluginGroupPermissions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PluginGroupPermissions_PluginPermissions_PermissionId",
+                        column: x => x.PermissionId,
+                        principalTable: "PluginPermissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PluginGroupPermissionVersions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation(autoIDGenStrategy, autoIDGenStrategyValue),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    UpdatedAt = table.Column<DateTime>(nullable: true),
+                    WorkflowState = table.Column<string>(maxLength: 255, nullable: true),
+                    CreatedByUserId = table.Column<int>(nullable: false),
+                    UpdatedByUserId = table.Column<int>(nullable: false),
+                    Version = table.Column<int>(nullable: false),
+                    GroupId = table.Column<int>(nullable: false),
+                    PermissionId = table.Column<int>(nullable: false),
+                    IsEnabled = table.Column<bool>(nullable: false),
+                    PluginGroupPermissionId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PluginGroupPermissionVersions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PluginGroupPermissionVersions_PluginPermissions_PermissionId",
+                        column: x => x.PermissionId,
+                        principalTable: "PluginPermissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PluginGroupPermissionVersions_PluginGroupPermissions_PluginGroupPermissionId",
+                        column: x => x.PluginGroupPermissionId,
+                        principalTable: "PluginGroupPermissions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PluginGroupPermissions_PermissionId",
+                table: "PluginGroupPermissions",
+                column: "PermissionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PluginGroupPermissionVersions_PermissionId",
+                table: "PluginGroupPermissionVersions",
+                column: "PermissionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PluginGroupPermissionVersions_PluginGroupPermissionId",
+                table: "PluginGroupPermissionVersions",
+                column: "PluginGroupPermissionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CalendarUsers");
+
+            migrationBuilder.DropTable(
+                name: "CalendarUserVersions");
+
+            migrationBuilder.DropTable(
+                name: "PluginConfigurationValues");
+
+            migrationBuilder.DropTable(
+                name: "PluginConfigurationValueVersions");
+
+            migrationBuilder.DropTable(
+                name: "PluginGroupPermissionVersions");
+
+            migrationBuilder.DropTable(
+                name: "PluginGroupPermissions");
+
+            migrationBuilder.DropTable(
+                name: "PluginPermissions");
+        }
+    }
+}
